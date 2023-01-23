9ae5439c8045cf124285ac0d38f2080da9517c3c
CSUltiGod99 <80710483+CSUltiGod99@users.noreply.github.com>
2023-01-23 16:28:44 +0000

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,50 @@
+// CS - Unity - Create a Collider Detection:
+
+using System.Collections;
+using System.Collections.Generic;
+using UntiyEngine;
+
+public class enemyCollider : MonoBehaviour
+{
+     public Light mainLight;
+     public Light detectionLight; // this will be used to define each object by 
+  // the colour of the Light.
+
+     public GameObject enemySetTarget;
+     public Transform enemyTargetLocation;
+     public Vector3 enemyTargPosition;
+
+    /*
+  
+
+
+
+    void Update()
+    {
+  
+      if(
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+
+}
